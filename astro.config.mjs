46ceff78a2dd82f3143f026f367679b5c7e8d8ce
Raffaele <raffaele@stripedcodex.com>
2026-04-01 12:29:01 +0200

    timeout build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
