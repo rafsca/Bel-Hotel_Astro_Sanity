85f90f71df8ccb5c0eba0466e2bf35289d43eab7
Raffaele <raffaele@stripedcodex.com>
2026-04-08 09:41:22 +0200

    studio rout fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,29 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sanityIntegration from '@sanity/astro';
 import vercel from '@astrojs/vercel';
+
+const projectId =
+    process.env.SANITY_STUDIO_PROJECT_ID ||
+    process.env.PUBLIC_SANITY_PROJECT_ID ||
+    'vz99cd3t';
+
+const dataset =
+    process.env.SANITY_STUDIO_DATASET ||
+    process.env.PUBLIC_SANITY_DATASET ||
+    'production';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
     adapter: vercel(),
-    integrations: [react()],
+    integrations: [react(),  sanityIntegration({
+            projectId,
+            dataset,
+      useCdn: false,
+      studioBasePath: '/studio'
+    })],
 
     server: {
         port: 3000,
